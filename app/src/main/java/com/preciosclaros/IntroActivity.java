ed9a5a390b532fde732a0f3c39f9e7fa3b9a2f0f
lucas <lucas.depetris@awsoftware.com.ar>
2018-05-29 09:41:35 -0300

    - Cambio color de fondo de el tutorial

--- app/src/main/java/com/preciosclaros/IntroActivity.java
+++ app/src/main/java/com/preciosclaros/IntroActivity.java
@@ -21,9 +21,9 @@ public class IntroActivity extends AppIntro {
 
         // Instead of fragments, you can also use our default slide
         // Just set a title, description, background and image. AppIntro will do the rest.
-        addSlide(AppIntroFragment.newInstance("Busca Productos", "Busca un producto mediante su nombre o escaneando su codigo de barra.",R.drawable.buscar_product, getResources().getColor(R.color.colorPrimaryLight)));
-        addSlide(AppIntroFragment.newInstance("Ahorra Dinero", "Elegpur++i el mejor precio segun tus prioridades!",R.drawable.save_money_intro, getResources().getColor(R.color.colorPrimaryLight)));
-        addSlide(AppIntroFragment.newInstance("Ubicación", "Busca los productos cercanos al punto que elijas.",R.drawable.gps_intro_circle, getResources().getColor(R.color.colorPrimaryLight)));
+        addSlide(AppIntroFragment.newInstance("Busca Productos", "Busca un producto mediante su nombre o escaneando su codigo de barra.",R.drawable.buscar_product, getResources().getColor(R.color.grayBackground)));
+        addSlide(AppIntroFragment.newInstance("Ahorra Dinero", "Elegpur++i el mejor precio segun tus prioridades!",R.drawable.save_money_intro, getResources().getColor(R.color.grayBackground)));
+        addSlide(AppIntroFragment.newInstance("Ubicación", "Busca los productos cercanos al punto que elijas.",R.drawable.gps_intro_circle, getResources().getColor(R.color.grayBackground)));
 
         // OPTIONAL METHODS
         // Override bar/separator color.
